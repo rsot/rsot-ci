6907d25c9e270f82059b5f0fade0a1fee1f024f6
higuchi <tatsuya.th.higuchi@nts.ricoh.co.jp>
2017-05-23 18:48:24 +0900

    テスト用Javaファイル

---
+++ AppTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+ 
+public class AppTest {
+  @Test public void testSomeLibraryMethod() {
+    assertEquals("Hello Gradle", App.getMessage());
+  }
+}
