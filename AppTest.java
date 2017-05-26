932473fa7c754038e1aee16f6f6c91e7e4084a4f
higuchi <tatsuya.th.higuchi@nts.ricoh.co.jp>
2017-05-26 16:55:29 +0900

    !の追加

--- AppTest.java
+++ AppTest.java
@@ -3,6 +3,6 @@ import static org.junit.Assert.*;
  
 public class AppTest {
   @Test public void testSomeLibraryMethod() {
-    assertEquals("Hello Gradle", App.getMessage());
+    assertEquals("Hello Gradle!", App.getMessage());
   }
 }
