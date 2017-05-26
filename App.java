c4ea9a3e4ea90fef18030112d98f5b0216d0db92
higuchi <tatsuya.th.higuchi@nts.ricoh.co.jp>
2017-05-26 17:06:35 +0900

    !の削除

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
   public static String getMessage() {
-    return "Hello Gradle!";
+    return "Hello Gradle";
   }
   public static void main(String[] args) {
     System.out.println(App.getMessage());
--- AppTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
- 
-public class AppTest {
-  @Test public void testSomeLibraryMethod() {
-    assertEquals("Hello Gradle!", App.getMessage());
-  }
-}
